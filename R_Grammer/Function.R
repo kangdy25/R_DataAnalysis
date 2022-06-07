dd44a25423a5f9a98eca5bc54156af76d12986cc
Shineast <canfrog@naver.com>
2022-06-07 21:43:58 +0900

    Learn Function in R

---
+++ R_Grammer/Function.R
@@ -0,0 +1,18 @@
+x <- c(1, 2, 3)
+x
+
+mean(x)
+max(x)
+min(x)
+
+str = c("Hello", "World!", "is", "good!")
+str
+
+paste(str, collapse = ",") # 쉼표를 구분자로 str의 단어들 하나로 합치기
+paste(str, collapse = " ") 
+
+x_mean <- mean(x)
+x_mean
+
+str_paste <- paste(str, collapse = " ")
+str_paste
