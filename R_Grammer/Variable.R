4aa0beff4ddcb67b21246d43f65490b7bb631019
Shineast <canfrog@naver.com>
2022-06-07 21:35:07 +0900

    Learn Variable in R

---
+++ R_Grammer/Variable.R
@@ -0,0 +1,47 @@
+a <- 1
+a
+
+b <- 2
+b
+
+a + b
+a * b
+
+var1 <- c(1, 2, 5, 7, 8)
+# 이때 c함수는 combine의 약자입니다.
+var1
+
+var2 <- c(1:5)
+var2
+
+var3 <- seq(1, 5)
+# 이때 seq함수는 sequence의 약자입니다.
+var3
+
+var4 <- seq(1, 10, by = 2)
+var4
+
+var5 <- seq(1, 10, by = 3)
+var5
+
+var1
+var1 + 2
+
+var1
+var2
+var1 + var2
+
+str1 <- "a"
+str1
+
+str2 <- "text"
+str2
+
+str3 <- "Hello World"
+str3
+
+str4 <- c("a", "b", "c")
+str4
+
+str5 <- c("Hello", "World!", "is", "good!")
+str5
