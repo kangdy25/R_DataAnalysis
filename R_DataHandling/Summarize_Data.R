de958ea18701b980e970f83c4693b5723a8f4152
Kangdy <canfrog@naver.com>
2022-06-15 18:56:34 +0900

    Learn Summarize Data

---
+++ R_DataHandling/Summarize_Data.R
@@ -0,0 +1,25 @@
+exam %>% summarise(mean_math = mean(math)) # Averaging Mean
+
+exam %>%
+  group_by(class) %>% #Seperate by group
+  summarise(mean_math = mean(math))
+
+exam %>%
+  group_by(class) %>%
+  summarise(mean_math = mean(math),
+            sum_math = sum(math),
+            median_math = median(math),
+            n = n())
+
+mpg %>% 
+  group_by(manufacturer, drv) %>%
+  summarise(mean_cty = mean(cty)) %>%
+  head(10)
+
+mpg %>%
+  group_by(manufacturer) %>%
+  filter(class == "suv") %>%
+  mutate(tot = (cty + hwy) / 2) %>%
+  summarise(mean_tot = mean(tot)) %>%
+  arrange(desc(mean_tot)) %>%
+  head(5)
