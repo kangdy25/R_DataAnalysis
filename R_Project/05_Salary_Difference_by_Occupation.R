4a578d86cd211e912fe7e855dad7ed0b323ac8b1
Kangdy <canfrog@naver.com>
2022-06-24 21:40:11 +0900

    Fifth Data Analysis Project by using R

---
+++ R_Project/05_Salary_Difference_by_Occupation.R
@@ -0,0 +1,94 @@
+install.packages("foreign")
+
+library(foreign)
+library(dplyr)
+library(ggplot2)
+library(readxl)
+
+# Import Data
+raw_welfare <- read.spss(file = "Koweps_hpc10_2015_beta1.sav",
+                         to.data.frame = T)
+# Make Copy
+welfare <- raw_welfare
+
+
+head(welfare)
+tail(welfare)
+View(welfare)
+dim(welfare)
+str(welfare)
+summary(welfare)
+
+welfare <- rename(welfare,
+                  sex = h10_g3,
+                  birth = h10_g4,
+                  marriage = h10_g10,
+                  religion = h10_g11,
+                  income = p1002_8aq1,
+                  code_job = h10_eco9,
+                  c0de_region = h10_reg7)
+
+# -------------------------------------------------------------- #
+
+class(welfare$income)
+summary(welfare$income)
+qplot(welfare$income)
+qplot(welfare$income) + xlim(0, 1000)
+
+# 이상치 확인
+summary(welfare$income)
+
+# 이상치 결측 처리
+welfare$income <- ifelse(welfare$income %in% c(0, 9999), NA, welfare$income)
+
+# 결측치 확인
+table(is.na(welfare$income))
+
+# -------------------------------------------------------------- #
+
+class(welfare$code_job)
+table(welfare$code_job)
+
+list_job <- read_excel("Koweps_Codebook.xlsx", col_names = T, sheet = 2)
+head(list_job)
+dim(list_job)
+
+welfare <- left_join(welfare, list_job, id = "code_job")
+
+welfare %>%
+  filter(!is.na(code_job)) %>%
+  select(code_job, job) %>%
+  head(10)
+
+# -------------------------------------------------------------- #
+
+job_income <- welfare %>%
+  filter(!is.na(job) & !is.na(income)) %>%
+  group_by(job) %>%
+  summarise(mean_income = mean(income))
+
+head(job_income)
+
+# 상위 10개 추출
+
+top10 <- job_income %>%
+  arrange(desc(mean_income)) %>%
+  head(10)
+
+top10
+
+ggplot(data = top10, aes(x = reorder(job, mean_income), y = mean_income)) +
+  geom_col() +
+  coord_flip()
+
+# 하위 10개 추출
+bottom10 <- job_income %>%
+  arrange(mean_income) %>%
+  head(10)
+
+bottom10
+
+ggplot(data = bottom10, aes(x = reorder(job, -mean_income), y = mean_income)) +
+  geom_col() +
+  coord_flip() +
+  ylim(0,850)
